f0666af4f1f4a82e044bb55d4ce57eb8b40e6661
alberto-pizzi <alberto.pizzi2002@outlook.com>
2024-03-09 16:52:41 +0100

    Risolti alcuni bug relativi agli indici

--- heap_implementation.py
+++ heap_implementation.py
@@ -24,16 +24,17 @@ class Maxheap:
             self.heap[self.size] = value
             self.heapIncreaseKey(self.size,value)
 
-    def parent(self,i):
+    #Return position of i's parent
+    def parentPos(self,i):
         if self.size > 0:
-            return (self.)
+            return math.floor(i/2)
 
     def heapIncreaseKey(self,i,value):
         if value < self.heap[i]:
             print("New key is lower than the old one")
             return
         self.heap[i] = value
-        while (i > 1) and (self.heap[math.floor(i/2)]):
-            self.heap[i],self.heap[self.parent(i)] = swap(self.heap[i],self.heap[self.parent(i)])
-            i = self.parent(i)
+        while (i > 0) and (self.heap[self.parentPos(i)]):
+            self.heap[i],self.heap[self.parentPos(i)] = swap(self.heap[i],self.heap[self.parentPos(i)])
+            i = self.parentPos(i)
 

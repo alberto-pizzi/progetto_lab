e25600db9e8945ac7fec78054e7a387137352899
alberto-pizzi <alberto.pizzi2002@outlook.com>
2024-03-11 18:26:05 +0100

    Fatto pulizia con eredetarità

--- heap_implementation.py
+++ heap_implementation.py
@@ -25,14 +25,43 @@ class Heap:
         print ("Print heap: ")
         print(self.heap, end=" ")
 
-class Maxheap(Heap):
-    def heapMaximum(self):
+    def heapify(self,pos):
+        pass
+    def editKey(self,i,value):
+        pass
+    def extract(self):
+        if self.size < 1:
+            return None
+        rootValue = self.heap[0]
+        self.heap[0] = self.heap[self.size-1]
+        self.size -= 1
+        self.heapify(0) #FIXME
+        return rootValue
+    def buildHeap(self,a):
+        self.size = len(a)
+        self.heap = a
+        start_index = (len(a)//2)-1
+        for i in range(start_index,-1,-1):
+            self.heapify(i)
+    #return max or min value of the heap
+    def rootPop(self):
         if self.size > 0:
             return self.heap[0]
         else:
             print("Heap is empty")
             return
-    def heapIncreaseKey(self,i,value):
+
+    #FIXME overflow index
+    def insert(self, value):
+            if self.size >= self.maxsize:
+                print("Heap is full")
+                return
+            self.size += 1
+            self.heap[self.size-1] = value
+            self.editKey(self.size - 1, value)
+
+class Maxheap(Heap):
+    def editKey(self, i, value):
         if value < self.heap[i]:
             print("New key is lower than the old one")
             return
@@ -40,17 +69,7 @@ class Maxheap(Heap):
         while (i > 0) and (self.heap[self.parentPos(i)]):
             self.heap[i],self.heap[self.parentPos(i)] = swap(self.heap[i],self.heap[self.parentPos(i)])
             i = self.parentPos(i)
-
-#FIXME overflow index
-    def heapInsert(self,value):
-            if self.size >= self.maxsize:
-                print("Heap is full")
-                return
-            self.size += 1
-            self.heap[self.size-1] = value
-            self.heapIncreaseKey(self.size-1,value)
-
-    def maxHeapify(self,pos):
+    def heapify(self, pos):
         left = self.leftChildPos(pos)
         right = self.rightChildPos(pos)
         if (left < self.size) and (self.heap[left] > self.heap[pos]):
@@ -61,23 +80,7 @@ class Maxheap(Heap):
             maxValuePos = right
         if maxValuePos != pos:
             self.heap[pos],self.heap[maxValuePos] = swap(self.heap[pos],self.heap[maxValuePos])
-            self.maxHeapify(maxValuePos)
-
-    def buildMaxHeap(self,a):
-        self.size = len(a)
-        self.heap = a
-        start_index = math.floor(len(a)/2)-1
-        for i in range(start_index,-1,-1):
-            self.maxHeapify(i)
-
-    def heapExtractMax(self):
-        if self.size < 1:
-            return None
-        maxValue = self.heap[0]
-        self.heap[0] = self.heap[self.size-1]
-        self.size -= 1
-        self.maxHeapify(0) #FIXME
-        return maxValue
+            self.heapify(maxValuePos)
 
 #TODO implment it for min-heap
     #optimized os-rank with O(rank)
@@ -100,6 +103,29 @@ class Maxheap(Heap):
         else:
             return 0
 
+class Minheap(Heap):
+    def editKey(self, i, value):
+        if value > self.heap[i]:
+            print("New key is bigger than the old one")
+            return
+        self.heap[i] = value
+        while (i > 0) and (self.heap[self.parentPos(i)]):
+            self.heap[i],self.heap[self.parentPos(i)] = swap(self.heap[i],self.heap[self.parentPos(i)])
+            i = self.parentPos(i)
+
+    def heapify(self, pos):
+        left = self.leftChildPos(pos)
+        right = self.rightChildPos(pos)
+        if (left < self.size) and (self.heap[left] < self.heap[pos]):
+            minValuePos = left
+        else:
+            minValuePos = pos
+        if (right < self.size) and (self.heap[right] < self.heap[minValuePos]):
+            minValuePos = right
+        if minValuePos != pos:
+            self.heap[pos],self.heap[minValuePos] = swap(self.heap[pos],self.heap[minValuePos])
+            self.heapify(minValuePos)
+
 
 #TODO remove these tests
 """
@@ -110,7 +136,7 @@ print("Stampo array: ",arr)
 h1 = Maxheap(10)
 #arr = np.array([8,5,10,3,12,7])
 arr = np.array([38,203,1,45,39,10,34,90,10,2,100,1])
-h1.buildMaxHeap(arr)
+h1.buildHeap(arr)
 h1.printHeap()
 """
 while h1.heapMaximum() != 39:

52f72215953d230238edf32edadf936511ad856c
alberto-pizzi <alberto.pizzi2002@outlook.com>
2024-03-11 11:59:13 +0100

    Implementati i metodi maxElement e minElement

--- list_implementation.py
+++ list_implementation.py
@@ -16,12 +16,42 @@ class LinkedList:
         while currentNode:
             print(currentNode.value)
             currentNode = currentNode.next
-
+    #FIXME improve value not found case
+    def searchElement(self, value):
+        currentNode = self.head
+        while currentNode.value != None and currentNode.value != value:
+            currentNode = currentNode.next
+        return currentNode.value
+    def maxElement(self):
+        if not self.head:
+            return None
+        max = self.head.value
+        currentNode = self.head.next
+        while currentNode:
+            if currentNode.value > max:
+                max = currentNode.value
+            currentNode = currentNode.next
+        return max
+    def minElement(self):
+        if not self.head:
+            return None
+        min = self.head.value
+        currentNode = self.head.next
+        while currentNode:
+            if currentNode.value < min:
+                min = currentNode.value
+            currentNode = currentNode.next
+        return min
 
 
 #Testing implementation
 myList = LinkedList()
+myList.addElement(30)
 myList.addElement(10)
 myList.addElement(12)
 myList.addElement(15)
 myList.printList()
+print("trova max:")
+print(myList.maxElement())
+print("trova min:")
+print(myList.minElement())

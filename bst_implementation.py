bf67f27b0234ccc352a60a6d1e47bf1b1cf97f49
alberto-pizzi <alberto.pizzi2002@outlook.com>
2024-03-13 21:55:07 +0100

    ABR: Implementati max,min,attraversamenti, succ,pred,delete. Bug fix.

--- bst_implementation.py
+++ bst_implementation.py
@@ -5,43 +5,26 @@ class BSTNode:
         self.value = value
         self.parent = None
 
-    # TODO rimuovere se necessario
-    def getValue(self):
-        return self.value
-    def setValue(self,value):
-        self.value = value
-    def getLeftChild(self):
-        return self.left
-    def setLeftChild(self,value):
-        self.left = value
-    def getRightChild(self):
-        return self.right
-    def setRightChild(self, value):
-        self.right = value
-
 class BST:
     def __init__(self):
         self.root = None
-    # TODO rimuovere se necessario
-    def getRoot(self):
-        return self.root
 
     def inorderTraversal(self,root):
         if root is not None:
             self.inorderTraversal(root.left)
-            print(root.getValue(), end=' ')
+            print(root.value, end=' ')
             self.inorderTraversal(root.right)
 
     def preorderTraversal(self, root):
         if root is not None:
-            print(root.getValue(), end=' ')
+            print(root.value, end=' ')
             self.inorderTraversal(root.left)
             self.inorderTraversal(root.right)
     def postorderTraversal(self, root):
         if root is not None:
             self.inorderTraversal(root.left)
             self.inorderTraversal(root.right)
-            print(root.getValue(), end=' ')
+            print(root.value, end=' ')
 
     def insertElement(self,value):
         newNode = BSTNode(value)
@@ -56,6 +39,7 @@ class BST:
                 tmp = tmp.left
             else:
                 tmp = tmp.right
+        newNode.parent = prev
         if (value < prev.value):
             prev.left = newNode
         else:
@@ -72,29 +56,71 @@ class BST:
                 currentNode = currentNode.right
         return False
 
+    def searchNode(self,value):
+        currentNode = self.root
+        while currentNode is not None and value is not currentNode.value:
+            if value < currentNode.value:
+                currentNode = currentNode.left
+            else:
+                currentNode = currentNode.right
+        return currentNode
 
-    #FIXME
     def treeMinimum(self, subRoot):
         currentNode = subRoot
         while currentNode.left is not None:
             currentNode = currentNode.left
-        return currentNode.value
-    #FIXME
+        return currentNode
+
     def treeMaximum(self, subRoot):
         currentNode = subRoot
         while currentNode.right is not None:
             currentNode = currentNode.right
-        return currentNode.value
-    #FIXME
+        return currentNode
+
     def treeSuccessor(self, subRoot):
         currentNode = subRoot
         if currentNode.right is not None:
             return self.treeMinimum(currentNode.right)
         prev = currentNode.parent
         while prev is not None and currentNode is prev.right:
-            current = prev
+            currentNode = prev
             prev = prev.parent
         return prev
+    def treePredecessor(self, subRoot):
+        currentNode = subRoot
+        if currentNode.left is not None:
+            return self.treeMaximum(currentNode.left)
+        prev = currentNode.parent
+        while prev is not None and currentNode is prev.left:
+            currentNode = prev
+            prev = prev.parent
+        return prev
+    def transplant(self, oldRoot, newRoot):
+        if oldRoot.parent is None:
+            self.root = newRoot
+        elif oldRoot is oldRoot.parent.left:
+            oldRoot.parent.left = newRoot
+        else:
+            oldRoot.parent.right = newRoot
+        if newRoot is not None:
+            newRoot.parent = oldRoot.parent
+
+    def treeDelete(self,node):
+        if node.left is None:
+            self.transplant(node, node.right)
+        elif node.right is None:
+            self.transplant(node,node.left)
+        else:
+            y = self.treeMinimum(node.right)
+            if y.parent is not node:
+                self.transplant(y,y.right)
+                y.right = node.right
+                y.right.parent = y
+            self.transplant(node,y)
+            y.left = node.left
+            y.left.parent = y
+
+
 
 albero = BST()
 albero.insertElement(50)
@@ -112,8 +138,11 @@ print("\n Postorder: ")
 albero.postorderTraversal(albero.root)
 print("\n")
 print("60 è nel bst?",albero.treeSearch(60))
-print("il max è: ", albero.treeMaximum())
-print("il min è: ", albero.treeMinimum())
+print("il max è: ", albero.treeMaximum(albero.root).value)
+print("il min è: ", albero.treeMinimum(albero.root).value)
 
-#print("\nil successore di 30: ", albero.treeSuccessor(30))
+print("\nil predecessore di 40: ", albero.treePredecessor(albero.searchNode(40)).value)
+#print("nodo 40 :",albero.searchNode(40).value)
+#albero.treeDelete(albero.searchNode(40))
+#albero.inorderTraversal(albero.root)
 

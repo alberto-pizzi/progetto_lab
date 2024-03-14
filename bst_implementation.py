233b33d84d7bffcda5a75f74458a7e9c3f8df300
alberto-pizzi <alberto.pizzi2002@outlook.com>
2024-03-14 20:03:05 +0100

    Implementato metodo size, osSelect,osRank e median per ABR

--- bst_implementation.py
+++ bst_implementation.py
@@ -1,3 +1,4 @@
+import numpy as np
 class BSTNode:
     def __init__(self,value):
         self.left = None
@@ -47,7 +48,7 @@ class BST:
             prev.right = newNode
 
     # TODO remove if not useful
-    def treeSearch(self,value):
+    def searchValue(self, value):
         currentNode = self.root
         while currentNode is not None:
             if currentNode.value == value:
@@ -60,27 +61,27 @@ class BST:
 
     def searchNode(self,value):
         currentNode = self.root
-        while currentNode is not None and value is not currentNode.value:
+        while currentNode is not None and value != currentNode.value:
             if value < currentNode.value:
                 currentNode = currentNode.left
             else:
                 currentNode = currentNode.right
         return currentNode
 
-    def treeMinimum(self, subRoot):
-        currentNode = subRoot
+    def treeMinimum(self, node):
+        currentNode = node
         while currentNode.left is not None:
             currentNode = currentNode.left
         return currentNode
 
-    def treeMaximum(self, subRoot):
-        currentNode = subRoot
+    def treeMaximum(self, node):
+        currentNode = node
         while currentNode.right is not None:
             currentNode = currentNode.right
         return currentNode
 
-    def nodeSuccessor(self, subRoot):
-        currentNode = subRoot
+    def nodeSuccessor(self, node):
+        currentNode = node
         if currentNode.right is not None:
             return self.treeMinimum(currentNode.right)
         prev = currentNode.parent
@@ -89,8 +90,8 @@ class BST:
             prev = prev.parent
         return prev
 
-    def nodePredecessor(self, subRoot):
-        currentNode = subRoot
+    def nodePredecessor(self, node):
+        currentNode = node
         if currentNode.left is not None:
             return self.treeMaximum(currentNode.left)
         prev = currentNode.parent
@@ -99,28 +100,28 @@ class BST:
             prev = prev.parent
         return prev
 
-    def transplant(self, oldRoot, newRoot):
-        if oldRoot.parent is None:
-            self.root = newRoot
-        elif oldRoot is oldRoot.parent.left:
-            oldRoot.parent.left = newRoot
+    def transplant(self, oldRootNode, newRootNode):
+        if oldRootNode.parent is None:
+            self.root = newRootNode
+        elif oldRootNode is oldRootNode.parent.left:
+            oldRootNode.parent.left = newRootNode
         else:
-            oldRoot.parent.right = newRoot
-        if newRoot is not None:
-            newRoot.parent = oldRoot.parent
+            oldRootNode.parent.right = newRootNode
+        if newRootNode is not None:
+            newRootNode.parent = oldRootNode.parent
 
     def deleteNode(self, node):
         if node.left is None:
             self.transplant(node, node.right)
         elif node.right is None:
-            self.transplant(node,node.left)
+            self.transplant(node, node.left)
         else:
             y = self.treeMinimum(node.right)
             if y.parent is not node:
-                self.transplant(y,y.right)
+                self.transplant(y, y.right)
                 y.right = node.right
                 y.right.parent = y
-            self.transplant(node,y)
+            self.transplant(node, y)
             y.left = node.left
             y.left.parent = y
 
@@ -128,18 +129,20 @@ class BST:
     def osSelectSmallest(self,root,k):
         currentNode = self.treeMinimum(root)
         for i in range(k-1):
+            if currentNode is None:
+                return None
             currentNode = self.nodeSuccessor(currentNode)
-        if currentNode is None:
-            return None
+
         return currentNode.value
 
     # Finds k-th biggest value
     def osSelectBiggest(self,root,k):
         currentNode = self.treeMaximum(root)
         for i in range(k-1):
+            if currentNode is None:
+                return None
             currentNode = self.nodePredecessor(currentNode)
-        if currentNode is None:
-            return None
+
         return currentNode.value
 
     # TODO rimuovere se non si usa
@@ -147,8 +150,6 @@ class BST:
         if root is None:
             return 0
         return self.size(root.left) + self.size(root.right) + 1
-
-
 
     """
     def osRank(self,root,target):
@@ -172,21 +173,56 @@ class BST:
         if root.value <= target:
             return 1 + rankLeft + rankRight
         else:
-            return rankLeft + rankRight"""
+            return rankLeft + rankRight
 
     # FIXME per i valori non presente restituisce il rango più alto, invece di None
-    def osRank(self,node,target):
-        if node is None:
+    def osRank(self, target):
+        if target is None:
             return 0
         successor = self.nodeSuccessor(self.treeMinimum(self.root))
         rank = 1
-        while successor is not None and successor.value <= target:
+        while successor is not None and successor.value <= target.value:
             rank += 1
             successor = self.nodeSuccessor(successor)
         return rank
+"""
+
+    # Returns a node (pointer) with i-th smallest value
+    def osSelect(self,node,i):
+        rank = self.size(node.left) + 1
+        if i == rank:
+            return node
+        elif i < rank:
+            return self.osSelect(node.left, i)
+        else:
+            return self.osSelect(node.right, i - rank)
+
+    # Returns the rank (number) of node
+    def osRank(self, node):
+        # FIXME caso: valore non presente
+        if node is None:
+            return 0
+        rank = self.size(node.left) + 1
+        y = node
+        while y != self.root:
+            if y == y.parent.right:
+                rank += self.size(y.parent.left) + 1
+            y = y.parent
+        return rank
+
+    # Returns median value of BST
+    def median(self):
+        totalNodes = self.size(self.root)
+        if totalNodes % 2 == 0:
+            mid1 = totalNodes // 2
+            mid2 = mid1 + 1
+            return (self.osSelect(self.root, mid1).value + self.osSelect(self.root, mid2).value) / 2
+        else:
+            mid = (totalNodes + 1) // 2
+            return self.osSelect(self.root, mid).value
 
 albero = BST()
-
+"""
 albero.insertElement(50)
 albero.insertElement(30)
 albero.insertElement(20)
@@ -201,16 +237,24 @@ albero.preorderTraversal(albero.root)
 print("\n Postorder: ")
 albero.postorderTraversal(albero.root)
 print("\n")
-print("60 è nel bst?",albero.treeSearch(60))
+print("60 è nel bst?", albero.searchValue(60))
 print("il max è: ", albero.treeMaximum(albero.root).value)
 print("il min è: ", albero.treeMinimum(albero.root).value)
 
 print("\nil predecessore di 40: ", albero.nodePredecessor(albero.searchNode(40)).value)
+"""
+array1 = np.arange(0, 100, 13)
+# np.random.shuffle(array1)
+for value in array1:
+    albero.insertElement(value)
+#albero.insertElement(39)
+albero.inorderTraversal(albero.root)
+print("\nil 4 valore più piccolo è ", albero.osSelect(albero.root,4).value)
 
-print("il 7 valore più piccolo è ", albero.osSelectSmallest(albero.root,7))
 
-
-print("il rango di 20 è: ", albero.osRank(albero.root,20))
+print("\nil rango di 39 è: ", albero.osRank(albero.searchNode(39)))
+print("size di 0:", albero.size(albero.searchNode(39)))
+print("trova mediana:",albero.median())
 #print("nodo 40 :",albero.searchNode(40).value)
 #albero.treeDelete(albero.searchNode(40))
 #albero.inorderTraversal(albero.root)
